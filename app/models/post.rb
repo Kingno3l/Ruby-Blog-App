de7341b05c72cebd4eb721683703d8052dd99328
kingno3l <nuelluiz@gmail.com>
2023-12-15 11:45:12 +0100

    linters fixed

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,11 +11,3 @@ class Post < ApplicationRecord
     comments.order(created_at: :desc).limit(count)
   end
 end
-
-
-#    first_post = Post.create(author: first_user, title: 'Hello first user first post', text: 'This is my first post as a first user')
-#    second_post = Post.create(author: second_user, title: 'Hello second user first post', text: 'This is my first post as a second user')
-#    third_post = Post.create(author: first_user, title: 'Hello first user second post', text: 'This is my second post as a first user')
-#    fourth_post = Post.create(author: second_user, title: 'Hello second user second post', text: 'This is my second post as a second user')
-#    fifth_post = Post.create(author: first_user, title: 'Hello first user third post', text: 'This is my third post as a first user')
-#    sixth_post = Post.create(author: second_user, title: 'Hello second user third post', text: 'This is my third post as a second user')
