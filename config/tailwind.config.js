9743a5a0dc76892d53bec19800ebbbe32320584d
kingno3l <nuelluiz@gmail.com>
2024-01-13 22:42:48 +0100

    check

---
+++ config/tailwind.config.js
@@ -0,0 +1,28 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        "color-text": "#0D1282",
+        "color-backgroung": "#EEEDED",
+        "color-red": "#D71313",
+        "color-yellow": "#F0DE36"
+    },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
+  ]
+}
