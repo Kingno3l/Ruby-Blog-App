8dd36e1d03d22361435f3a0bc1e700f1266425eb
kingno3l <nuelluiz@gmail.com>
2024-01-13 22:37:00 +0100

    implemented

--- app/helpers/application_helper.rb
+++
@@ -1,3 +0,0 @@
-# Module to provide helper methods for the application views.
-module ApplicationHelper
-end
--- app/mailers/application_mailer.rb
+++
@@ -1,5 +0,0 @@
-# Base mailer class for the application.
-class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
-  layout 'mailer'
-end
