b15525c65c9999d0897cb908bf6cef926a399625
kingno3l <nuelluiz@gmail.com>
2024-01-19 11:35:55 +0100

    project implemented successfully

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,40 +1,26 @@
 class PostsController < ApplicationController
   def index
-    @user = User.includes(:posts).find_by(id: params[:user_id])
-    @posts = @user.posts.includes(:comments, :likes)
-  end
-
-  def new
-    post = Post.new
-    respond_to do |format|
-      format.html { render :new, locals: { post: } }
-    end
+    @user = User.includes(:posts).find(params[:user_id])
+    @posts = @user.posts.includes(:author, :likes, :comments)
   end
 
   def show
-    @user = User.find_by(id: params[:user_id])
-    @post = @user.posts.find(params[:id])
-    @current_user = current_user
-    @like = Like.new
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
   end
 
   def create
-    post = current_user.posts.new(post_params)
-
-    respond_to do |format|
-      format.html do
-        if post.save
-          redirect_to user_posts_url(user_id: current_user.id), notice: 'Post was successfully created.'
-        else
-          render :new
-        end
-      end
+    @post = current_user.posts.build(params.require(:post).permit(:title, :text))
+    if @post.save
+      flash[:success] = 'Post saved successfully'
+      redirect_to user_posts_path
+    else
+      flash.now[:error] = 'Post not saved, try again later'
+      puts puts @post.errors.full_messages
+      render :new
     end
-  end
-
-  private
-
-  def post_params
-    params.require(:post).permit(:title, :text)
   end
 end
